72fe4858c446600ea40feb4fc8d3fd9b8a36845f
Kazbek999 <kazbek999@yandex.ru>
2021-06-25 23:23:33 +1000

    initial commit

---
+++ Lesson3/Apple.java
@@ -0,0 +1,11 @@
+package LessonJC3;
+
+public class Apple extends Fruit {
+Apple() {
+    super(1.0f);
+}
+    @Override
+    public String toString(){
+        return "яблоки";
+}
+}
---
+++ Lesson3/Fruit.java
@@ -0,0 +1,11 @@
+package LessonJC3;
+
+abstract class Fruit {
+    private float weight;
+    Fruit (float weight) {
+        this.weight = weight;
+    }
+    public float getWeight() {
+       return weight;
+    }
+}
